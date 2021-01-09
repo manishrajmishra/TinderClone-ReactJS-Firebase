a9bccbac40393005f8ad1ecb3ac11912248e9bc1
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2021-01-09 23:38:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import "./styles.css";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Let's build Tinder Clone</h1>
+      {/* Header */}
+      {/* Tinder Cards */}
+      {/* Buttons below tinder cards */}
+
+      {/* Chats Screen */}
+      {/* Individual chat screen */}
     </div>
   );
 }
